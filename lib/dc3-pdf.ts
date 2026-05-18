190b8cdfbf17e2fe4b11080062fa33fe8c0b9da7
Jordan Payta <jpayta@uabc.edu.mx>
2026-05-18 15:40:19 -0700

    fix(dc3): adjust 'razonSocial' position for PDF generation

--- lib/dc3-pdf.ts
+++ lib/dc3-pdf.ts
@@ -12,7 +12,7 @@ const POS = {
   ocupacion:        { x: 305, y: 559, size: 9 },
   puesto:           { x: 40,  y: 535, size: 10 },
   // DATOS DE LA EMPRESA
-  razonSocial:      { x: 40,  y: 480, size: 10 },
+  razonSocial:      { x: 40,  y: 470, size: 10 },
   rfcStartX:        40,
   rfcY:             410,
   rfcStep:          13,
