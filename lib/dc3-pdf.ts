5079d5662f85e23fcac0ee590a689afebf2dd366
Jordan Payta <jpayta@uabc.edu.mx>
2026-05-19 08:31:30 -0700

    fix(dc3): adjust 'rfcY' position for PDF generation

--- lib/dc3-pdf.ts
+++ lib/dc3-pdf.ts
@@ -14,7 +14,7 @@ const POS = {
   // DATOS DE LA EMPRESA
   razonSocial:      { x: 40,  y: 475, size: 10 },
   rfcStartX:        32,
-  rfcY:             440,
+  rfcY:             445,
   rfcStep:          14,
   // DATOS DEL PROGRAMA
   curso:            { x: 60,  y: 374, size: 9  },  
