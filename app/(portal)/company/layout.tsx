6444a439769b788cd95b33dc40dcf590d6f150fa
Jordan Payta <jpayta@uabc.edu.mx>
2026-07-21 14:36:30 -0700

    rename: English identifiers in company/layout

--- app/(portal)/company/layout.tsx
+++ app/(portal)/company/layout.tsx
@@ -2,7 +2,7 @@ import { getSession } from "@/lib/session"
 import { getCompanyAccessStatus } from "@/lib/company-status"
 import { redirect } from "next/navigation"
 
-export default async function EmpresaLayout({
+export default async function CompanyLayout({
   children,
 }: {
   children: React.ReactNode
