ac0641ca78a00f2bb15c111c615477667bb2ed72
Jordan Payta <jpayta@uabc.edu.mx>
2026-08-27 10:42:47 -0700

    Redondear las esquinas derechas del sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -137,6 +137,8 @@ export default function Sidebar({
         fontFamily: SIDEBAR_FONT,
         bgcolor: "var(--sidebar-navy)",
         borderRight: "1px solid var(--sidebar-navy-border)",
+        borderTopRightRadius: 20,
+        borderBottomRightRadius: 20,
       }}
     >
       <Box
